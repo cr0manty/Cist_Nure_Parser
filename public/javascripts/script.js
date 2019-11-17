db95304d6c38070c11e4d2197a1a572b15f2dc0d
cr0manty <36458874+cr0manty@users.noreply.github.com>
2019-11-18 00:16:17 +0200

    small continue

---
+++ public/javascripts/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    if (data) {
+        $('#result_area').val(data);
+    }
+});
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
 const fs = require("fs");
 const http = require('http');
 
@@ -10,24 +9,34 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     const form = req.body;
-    const true_url = `http://cist.nure.ua/ias/app/tt/WEB_IAS_TT_GNR_RASP.GEN_GROUP_POTOK_RASP?ATypeDoc=3&Aid_group=6283377&Aid_potok=0&ADateStart=01.09.2019&ADateEnd=31.01.2020&AMultiWorkSheet=0`;
+    const true_url = `http://cist.nure.ua/ias/app/tt/WEB_IAS_TT_GNR_RASP.GEN_GROUP_POTOK_RASP?ATypeDoc=3&
+    Aid_group=6283377&Aid_potok=0&ADateStart=01.09.2019&ADateEnd=31.01.2020&AMultiWorkSheet=0`;
     const url = `http://cist.nure.ua/ias/app/tt/WEB_IAS_TT_GNR_RASP.GEN_GROUP_POTOK_RASP?ATypeDoc=3&Aid_group=${form.group_id}
         &Aid_potok=0&ADateStart=${form.date_start}&ADateEnd=${form.date_end}&AMultiWorkSheet=0`;
 
     const file = fs.createWriteStream("file.csv", 'utf8');
-    const requests = http.get(true_url, function (response) {
+    http.get(true_url, function (response) {
         response.pipe(file);
     });
 
-    request(true_url, function (err, res, body) {
-        if (body) {
+    fs.readFile("file.csv", 'latin1', function (err, data) {
+        if (!err && data) {
+            req.sesion.csv = data;
+            res.redirect('/result');
+        }
+        else {
+            res.redirect('/');
         }
     });
-    // if (form.group_id && form.date_start && form.date_end) {
-    //
-    // } else {
-    //     res.send(404);
-    // }
 });
+
+router.get('/result', function (req, res, next) {
+    if (res.sesion.csv) {
+        res.render('result', {csv: res.sesion.csv});
+    } else {
+        res.redirect('/')
+    }
+});
+
 
 module.exports = router;
